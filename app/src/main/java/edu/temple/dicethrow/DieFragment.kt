c6ecf670d4ba73038a0eb281a819fb5f4c76ab9d
FudaylHopkins <tup92982@temple.edu>
2026-03-17 12:08:42 -0400

    implemnted on saved instance state

--- app/src/main/java/edu/temple/dicethrow/DieFragment.kt
+++ app/src/main/java/edu/temple/dicethrow/DieFragment.kt
@@ -12,7 +12,11 @@ class DieFragment : Fragment() {
 
     val DIESIDE = "sidenumber"
 
+    val ROLL_KEY = "currentroll"
+
     lateinit var dieTextView: TextView
+
+    var currentRoll = 1
 
     var dieSides: Int = 6
 
@@ -23,6 +27,10 @@ class DieFragment : Fragment() {
                 dieSides = this
             }
         }
+        savedInstanceState?.run{
+            currentRoll = getInt(ROLL_KEY)
+        }
+
     }
 
     override fun onCreateView(
@@ -37,10 +45,22 @@ class DieFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        throwDie()
+        if(currentRoll == 0) {
+            throwDie()
+        }
+        else{
+            dieTextView.text = currentRoll.toString()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt(ROLL_KEY, currentRoll)
     }
 
     fun throwDie() {
-        dieTextView.text = Random.nextInt(1,dieSides + 1).toString()
+        currentRoll = Random.nextInt(1,dieSides + 1)
+        dieTextView.text = currentRoll.toString()
     }
 }
