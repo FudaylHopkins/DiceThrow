4404455874c7b2989929884361c0c3eaa75456aa
FudaylHopkins <tup92982@temple.edu>
2026-03-17 12:20:56 -0400

    Finished class activity

--- app/src/main/java/edu/temple/dicethrow/DieFragment.kt
+++ app/src/main/java/edu/temple/dicethrow/DieFragment.kt
@@ -63,4 +63,13 @@ class DieFragment : Fragment() {
         currentRoll = Random.nextInt(1,dieSides + 1)
         dieTextView.text = currentRoll.toString()
     }
+
+
+    companion object{
+        fun newInstance(sides:Int)= DieFragment().apply {
+            arguments = Bundle().apply {
+                putInt(DIESIDE,sides)
+            }
+        }
+    }
 }
