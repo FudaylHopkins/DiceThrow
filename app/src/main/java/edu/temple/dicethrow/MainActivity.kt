9df55f5d1c517bcf3531c629ae101763c1443f24
FudaylHopkins <tup92982@temple.edu>
2026-03-18 16:11:09 -0400

    Finished lab activity

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -8,23 +8,38 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var die1: DieFragment
+    private lateinit var die2: DieFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        if(supportFragmentManager.findFragmentById(R.id.fragmentContainerView)==null) {
-            supportFragmentManager
-                .beginTransaction()
-                .add(R.id.fragmentContainerView, DieFragment.newInstance(20))
+        if (supportFragmentManager.findFragmentById(R.id.fragmentContainerView) == null) {
+            die1 = DieFragment.newInstance(6)
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragmentContainerView, die1)
                 .commit()
+        } else {
+            die1 = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as DieFragment
         }
 
+        if (supportFragmentManager.findFragmentById(R.id.fragmentContainerView1) == null) {
+            die2 = DieFragment.newInstance(6)
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragmentContainerView1, die2)
+                .commit()
+        } else {
+            die2 = supportFragmentManager.findFragmentById(R.id.fragmentContainerView1) as DieFragment
+        }
+
+
+
         findViewById<Button>(R.id.rollDiceButton).setOnClickListener {
-            supportFragmentManager
-                .findFragmentById(R.id.fragmentContainerView)?.run {
-                    (this as DieFragment).throwDie()
-                }
+            die1.throwDie()
+            die2.throwDie()
         }
     }
 }
