016a433c128e216d2c50ae9bab31ff319f4128a4
FudaylHopkins <tup92982@temple.edu>
2026-03-19 11:20:00 -0400

    Created DieViewModel class

---
+++ app/src/main/java/edu/temple/dicethrow/DieViewModel.kt
@@ -0,0 +1,23 @@
+package edu.temple.dicethrow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlin.random.Random
+
+class DieViewModel : ViewModel() {
+
+    private val _currentRoll = MutableLiveData<Int>()
+    val currentRoll: LiveData<Int> get() = _currentRoll
+
+    var dieSides: Int = 6
+
+    fun setSides(sides:Int){
+        dieSides = sides
+    }
+
+    fun rollDie(){
+        _currentRoll.value = Random.nextInt(1, dieSides+1)
+    }
+
+}
